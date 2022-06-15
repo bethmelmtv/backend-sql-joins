8bb95a8a1ec132e62552cebb877d7b200b215327
Beth <bethmelmtv@gmail.com>
2022-06-15 15:42:03 -0700

    got 8th test to pass

--- exercise_8/exercise-8.sql
+++ exercise_8/exercise-8.sql
@@ -1,2 +1,11 @@
 -- find all film titles rented out by customers who live in the country of Peru
 -- Similar to exercise 7 with an additional join to get country name
+SELECT title
+FROM film 
+INNER JOIN inventory ON film.film_id = inventory.film_id
+INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+INNER JOIN customer ON customer.customer_id = rental.customer_id
+INNER JOIN address ON address.address_id = customer.address_id
+INNER JOIN city ON city.city_id = address.city_id
+INNER JOIN country ON city.country_id = country.country_id
+WHERE country = 'Peru'
